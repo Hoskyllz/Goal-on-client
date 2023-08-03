dbd7e6a19155f15a2b1f3df71925648536062cf0
Hoskyllz <olabayoa009@gmail.com>
2023-08-03 12:36:56 +0100

    first commit

---
+++ src/pages/Allgoals.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import GoalHeader from "../components/GoalHeader";
+import goals from "../data/goals";
+import Goal from "../components/Goal";
+import Loading from "../components/Loading"
+import { useFetch } from "../Hooks/useFetch";
+
+const Allgoals = () => {
+  // const [Goals, setGoals] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true)
+  // const getGoals = async () => {
+  //   try {
+  //     const res = await fetch('http://localhost:7000/api/goals')
+  //     const data = await res.json()
+  //     setIsLoading (false)
+      
+  //     setGoals(data.goals)
+
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+  // useEffect(() => {
+  //   getGoals()
+  // }, [])
+  const {
+    isLoading,
+    data: {goals: Goals},
+  } = useFetch ("https://olasgoal.onrender.com/api/goals")
+  
+  //loading state
+  //when the goal is empty
+  return (
+    <div className="container pb-3">
+      <GoalHeader heading="All Goals" />
+      {isLoading && <Loading/>}
+      <div>
+        <div>
+          {Goals &&
+            Goals.map((g) => {
+            return <Goal key={g._id} {...g} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Allgoals;
